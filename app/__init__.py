4fa236080dbe97f08dd36833a0e847c2c8b995ce
songofhawk <songofhawk@gmail.com>
2020-04-10 20:03:38 +0800

    初始化文件上传

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask
+import os
+
+from flask import Flask, session
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = os.urandom(24)
+# session(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,61 @@
+from typing import Set
+
 from app import app
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS: Set[str] = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+# app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello, World!"
+def index(name=None):
+    # return "Welcome to image handler!"
+    return render_template('upload.html', name=name)
+
+
+@app.route('/upload.html')
+def upload_html(name=None):
+    return render_template('upload.html', name=name)
+
+
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return '找不到文件块', 404
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return '上传成功',200
+    return
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
