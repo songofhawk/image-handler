418e3f9676aa4eb977645bb1fbe5dba2d1f9effd
songofhawk <songofhawk@gmail.com>
2020-04-16 19:23:11 +0800

    编辑用户信息可以正常使用了

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, login_manager
+from flask_login import UserMixin
+from hashlib import md5
 
 from app import db
 
@@ -11,6 +12,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -24,6 +27,11 @@ class User(UserMixin, db.Model):
     @classmethod
     def get(cls, user_id):
         return User.query.get(user_id)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
